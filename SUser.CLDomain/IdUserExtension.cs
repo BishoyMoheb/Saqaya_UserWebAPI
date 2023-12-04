009668c1f0c700d0b07c1d282c0d3a75565e236e
BishoyMoheb <75308234+BishoyMoheb@users.noreply.github.com>
2023-12-04 14:15:30 +0200

    Adding the solution files

---
+++ SUser.CLDomain/IdUserExtension.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace SUser.CLDomain
+{
+    public class IdUserExtension : IdentityUser
+    {
+        public new Guid Id { get; set; }
+        public bool IsMarketingConsent { get; set; }
+    }
+}
---
+++ SUser.CLDomain/MUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;//To use Key
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUser.CLDomain
+{
+    public class MUser
+    {
+        [Key]
+        public string UId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public bool IsMarketingConsent { get; set; }
+    }
+}
