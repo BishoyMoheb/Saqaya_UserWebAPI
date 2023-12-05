b23214153d010e304ca67d6717190a5875cdcaec
BishoyMoheb <75308234+BishoyMoheb@users.noreply.github.com>
2023-12-05 16:46:16 +0200

    Adding new changes
    For security of accessing data

---
+++ SUser.CLDomain/MJwtSTokens.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;//To use Key
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUser.CLDomain
+{
+    public class MJwtSTokens
+    {
+        [Key]
+        public string UserId { get; set; }
+        public string JwtSToken { get; set; }
+    }
+}
